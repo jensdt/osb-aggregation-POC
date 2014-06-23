9acb02bda44f4afff9877a7e77072176dd40fa40
Bert Roex <roexber@MacBook-Pro-van-Bert.local>
2014-06-23 14:23:28 +0200

    mongolab/localhost detection

--- db.js
+++ db.js
@@ -1,13 +1,17 @@
 /**
  * Created by roexber on 23/06/14.
  */
-var mongoose = require( 'mongoose' );
-var Schema   = mongoose.Schema;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var NmscSchema = new Schema({
-    name    : String,
-    code    : String
+    name: String,
+    code: String
 });
 
-mongoose.model( 'Nmsc', NmscSchema );
-mongoose.connect( 'mongodb://osb:osb@ds063168.mongolab.com:63168/osb-aggregation' );
+mongoose.model('Nmsc', NmscSchema);
+
+var mongoUri = process.env.MONGOLAB_URI ||
+    'mongodb://localhost/express';
+
+mongoose.connect(mongoUri);
