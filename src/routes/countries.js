e764f1c8b5a7dc487a3e422e94ab12375ce4483f
Bert Roex <roexber@MacBook-Pro-van-Bert.local>
2014-06-30 13:41:21 +0200

    country api v3

--- src/routes/countries.js
+++ src/routes/countries.js
@@ -2,7 +2,7 @@ var restify = require('restify'),
     winston = require('winston');
 
 var client = restify.createJsonClient({
-    url: 'http://mysterious-sands-1012.herokuapp.com:80',
+    url: 'http://powerful-refuge-3004.herokuapp.com:80',
     version: '~1.0'
 });
 
@@ -22,7 +22,7 @@ exports.getCountry = function (req, resp) {
 };
 
 exports.getNmscCountry = function (nmscId, callback) {
-    client.get('/api/countries/1', function (err, req, res, country) {
+    client.get('/api/countries/3cfd47a9-f164-4cea-b1a9-5bfd3d8029d3', function (err, req, res, country) {
         callback(null, country);
     });
 };
