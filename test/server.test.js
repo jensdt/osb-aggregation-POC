d5c5330e7a74139b818e54f1e16142e3364fc0f1
roexber <bert.roex@xt-i.com>
2014-07-01 10:24:54 +0200

    use should keyword in test cases

--- test/server.test.js
+++ test/server.test.js
@@ -5,13 +5,13 @@ var request = require("supertest"),
     app = require("../server").app,
     assert = require("chai").assert;
 
-it("Server can return a 200 response", function (done) {
+it("should return a 200 response", function (done) {
     request(app)
         .get("/")
         .expect(200, done);
 });
 
-it("Server returns 'OSB Aggregation POC' on base path", function (done) {
+it("should return 'OSB Aggregation POC' on base path", function (done) {
     request(app)
         .get("/")
         .end(function (err, res) {
